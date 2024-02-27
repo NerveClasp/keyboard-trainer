843f6152d035fa8f938de807891394996560128f
Romka Horokhov <7347253+NerveClasp@users.noreply.github.com>
2024-02-27 21:02:36 +0200

    migrate to kit2

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# Colemak DHm Keyboard Trainer 
-WIP
+# create-svelte
 
-## Twitch
-I'm planning to stream writing this on [Twitch](https://twitch.tv/beardy_coding) at around 7pm - 8pm [EET (GMT+2)](https://www.timeanddate.com/time/zone/ukraine/kyiv) which is 5pm GMT, 12pm EST, and 9am PST [(learn more)](https://www.timeanddate.com/worldclock/converter.html?iso=20220105T170000&p1=367&p2=136&p3=179&p4=137)
-
-## Inspired by
-- [https://www.colemak.academy/](https://www.colemak.academy/)
-- [https://monkeytype.com/](https://monkeytype.com/)
-
-## create-svelte
-
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte);
+Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/main/packages/create-svelte).
 
 ## Creating a project
 
@@ -18,13 +8,11 @@ If you're seeing this, you've probably already done this step. Congrats!
 
 ```bash
 # create a new project in the current directory
-npm init svelte@next
+npm create svelte@latest
 
 # create a new project in my-app
-npm init svelte@next my-app
+npm create svelte@latest my-app
 ```
-
-> Note: the `@next` is temporary
 
 ## Developing
 
@@ -39,10 +27,12 @@ npm run dev -- --open
 
 ## Building
 
-Before creating a production version of your app, install an [adapter](https://kit.svelte.dev/docs#adapters) for your target environment. Then:
+To create a production version of your app:
 
 ```bash
 npm run build
 ```
 
-> You can preview the built app with `npm run preview`, regardless of whether you installed an adapter. This should _not_ be used to serve your app in production.
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ src/routes/trainer/+page.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+	import Keyboard from '$lib/keyboard/Keyboard.svelte';
+	import Input from '$lib/input/Input.svelte';
+	import { currentIndex } from './stores.js';
+	import { onMount, onDestroy } from 'svelte';
+
+	let currentIndexValue = 0;
+	const unsubscribe = currentIndex.subscribe((value) => {
+		currentIndexValue = value;
+	});
+	const text = 'qqййй third stream is done';
+	type Key = {
+		key: string;
+		status: 'default' | 'success' | 'mistake' | 'erased';
+		irrelevant?: boolean;
+	};
+	let textArray: Key[] = text.split('').map((key) => ({ key, status: 'default' }));
+	let userKey = '';
+	let currentKey = text[currentIndexValue];
+	const expectedInputRegex = /^(?:[\w ]|Backspace)$/gi;
+	let changed = false;
+	const MAX_USER_ERROR_KEYS = 20;
+	let userErrors = 0;
+	let done = false;
+
+	const onKeyDown = ({ key }) => {
+		if (expectedInputRegex.test(key)) {
+			userKey = key;
+			changed = true;
+		}
+	};
+
+	const onKeyUp = ({ key }) => {
+		if (expectedInputRegex.test(key)) {
+			userKey = '';
+		}
+	};
+
+	const updateCurrentKey = (index: number) => {
+		if (index >= 0 && index < textArray.length + 1) {
+			currentKey = text[index];
+		}
+	};
+
+	const updateIndex = (index: number) => {
+		if (index >= 0 && index < textArray.length + 1) {
+			currentIndex.set(index);
+		}
+	};
+
+	$: if (changed && !done && currentIndexValue <= textArray.length) {
+		if (userKey === 'Backspace') {
+			const newIndex = currentIndexValue - 1;
+			textArray[newIndex].status = 'erased';
+			updateIndex(newIndex);
+			if (textArray[newIndex].irrelevant) {
+				textArray.splice(newIndex, 1);
+			} else {
+				updateCurrentKey(newIndex);
+			}
+
+			changed = false;
+		} else {
+			const { key } = textArray?.[currentIndexValue] ?? {};
+			const success = key === userKey;
+			if (key) {
+				textArray[currentIndexValue].status = success ? 'success' : 'mistake';
+			}
+			let newIndex = currentIndexValue + 1;
+
+			if (success) {
+				updateCurrentKey(newIndex);
+				updateIndex(newIndex);
+				if (newIndex === textArray.length) {
+					done = true;
+				}
+			} else if ((key === ' ' || !key) && userErrors < MAX_USER_ERROR_KEYS) {
+				textArray.splice(currentIndexValue, 0, {
+					key: userKey,
+					status: 'mistake',
+					irrelevant: true
+				});
+				userErrors += 1;
+				updateIndex(newIndex);
+			} else if (userErrors < MAX_USER_ERROR_KEYS) {
+				updateIndex(newIndex);
+				updateCurrentKey(newIndex);
+			}
+
+			changed = false;
+		}
+	}
+
+	onMount(() => {
+		textArray = text.split('').map((key) => ({ key, status: 'default' }));
+
+		if (typeof window !== 'undefined') {
+			document.addEventListener('keydown', onKeyDown);
+			document.addEventListener('keydown', onKeyUp);
+		}
+	});
+	onDestroy(() => {
+		if (typeof window !== 'undefined') {
+			document.removeEventListener('keydown', onKeyDown);
+			document.removeEventListener('keydown', onKeyUp);
+		}
+		unsubscribe();
+	});
+</script>
+
+<div class="content">
+	<p>Trainer</p>
+	<div><Input currentIndex={currentIndexValue} {textArray} /></div>
+	<div><Keyboard {currentKey} /></div>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
